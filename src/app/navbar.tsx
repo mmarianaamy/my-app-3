d04d1ba9276f8b26dfb3ba2e6c673e6491bbb6c2
Mariana Amy <MartinezNevarezMarianaA@JohnDeere.com>
2025-02-20 12:42:36 -0600

    Actividad diaria

---
+++ src/app/navbar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NavBar () {
+    return (
+        <div className="flex flex-row justify-evenly bg-gray-800 p-6">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/page2"}>Page 2</Link>
+        </div>
+    )
+}
