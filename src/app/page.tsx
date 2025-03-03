d1d1da16d266e60eacab93422d3df57ba738f3f9
Mariana Amy <MartinezNevarezMarianaA@JohnDeere.com>
2025-03-03 11:20:47 -0600

    Weather

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,79 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { useFetchData } from "./hooks/getInfo";
+
+interface dataProps {
+  data: datadata,
+  loading: boolean,
+  error: errordata
+}
+
+interface errordata {
+  code: number
+  message: string
+}
+
+interface datadata {
+  location: locationdata
+  current: currentdata
+}
+
+interface locationdata {
+  name: string
+  country: string
+}
+
+interface currentdata {
+  last_updated: string,
+  temp_c: number,
+  condition: conditiondata,
+  wind_kph: number,
+  feelslike_c: number
+}
+
+interface conditiondata {
+  icon: string
+}
 
 export default function Home() {
 
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState("");
 
-  const handleSubmit = () => {
-    setValue(value + 1);
-  }
+  const [lookup, setLookup] = useState("")
 
-  const handleSubtract = () => {
-    setValue(value - 1);
-  }
+  const data : dataProps = useFetchData("https://api.weatherapi.com/v1/current.json?q=" + lookup + "&key=b5f3dbb46c5245faacf184355252602")
+
+  useEffect(()=>{console.log(data)}, [data])
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col justify-items-center text-center gap-8 min align-center">
-        
-        <p>{value}</p>
-
-        <div>
-        <button onClick={handleSubmit} className="p-3 m-5 border border-solid border-gray-600 rounded-md hover:bg-gray-800">Add</button>
-        <button onClick={handleSubtract} className="p-3 m-5 border border-solid border-gray-600 rounded-md hover:bg-gray-800">Subtract</button>
+      <div className="flex gap-3 w-3/4 text-black">
+        <input className="p-4 w-full nooutline rounded-lg" value={value} onChange={(e) => setValue(e.target.value)}></input>
+        <button className="text-white p-4 border border-white rounded-lg" onClick={() => setLookup(value)}>Search</button>
+      </div>
+      {data.loading && 
+      <div>
+        <p>Loading...</p>
+      </div>}
+      {!data.data.location && 
+      <div className="rounded-md flex flex-col items-center justify-center w-1/5 text-center p-3 bg-red-300">
+        <p>Error: {data.error.message} Try Again.</p>
+      </div>
+      }
+      {data.data.location && 
+        <div className="rounded-md flex flex-col items-center justify-center w-1/5 text-center p-3 bg-indigo-900">
+          <h3 className="text-5xl col-span-3">{data.data.location.name}</h3>
+          <p>{data.data.location.country}</p>
+          <p className="text-7xl p-5">{data.data.current.temp_c}°C</p>
+          <div className="flex items-center justify-center">
+            <img src={"https:" + data.data.current.condition.icon} alt="" width={100} height={100}/>
+            <p>Wind: {data.data.current.wind_kph}kph</p>
+            <p>Feels like: {data.data.current.feelslike_c}°C</p>
+          </div>
         </div>
-
-      </main>
+      }
     </div>
   );
 }
